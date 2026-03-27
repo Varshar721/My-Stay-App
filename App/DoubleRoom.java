e0abbc11d76c2829d49e90cb2f1150b611d8780a
Varsha721 <varshavr0021@gmail.com>
2026-03-27 10:44:40 +0530

    UC2:Basic Rooms

---
+++ App/DoubleRoom.java
@@ -0,0 +1,5 @@
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 300.0, 4500.0);
+    }
+}
---
+++ App/SuiteRoom.java
@@ -0,0 +1,5 @@
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 500.0, 8000.0);
+    }
+}
